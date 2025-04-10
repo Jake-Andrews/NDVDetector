ec90b11fd9d226543ebd8283101975198c71fee6
Jake-Andrews <jake.andrews1@ontariotechu.net>
2025-04-10 15:41:03 -0400

    Delete files that were renamed

--- src/searchfiles.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <filesystem>
-#include <unordered_set>
-#include <vector>
-
-std::vector<std::filesystem::path> get_files_with_extensions(std::filesystem::path const& root, std::unordered_set<std::string> const& extensions);
